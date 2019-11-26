e4653da9836dfd61d5312484b840410de9ab29d5
Matheus Santos <matheus.santos@ilegra.com>
2019-11-26 16:08:13 -0300

    Added Header component state and style.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,34 @@
-import React, { ReactElement } from 'react';
+import React, { Component } from 'react';
+import './Header.scss';
 
-const Header = (): ReactElement => {
-  return <header id="main-header">Hello</header>;
-};
+export default class Header extends Component {
+  state = {
+    routes: [
+      { path: '/', item: 'Home' },
+      { path: '/characters', item: 'Characters' },
+      { path: '/planets', item: 'Planets' },
+      { path: '/species', item: 'Species' },
+      { path: '/starships', item: 'Starships' },
+      { path: '/vehicles', item: 'Vehicles' },
+      { path: '/about', item: 'About' }
+    ]
+  };
 
-export default Header;
+  render() {
+    const { routes } = this.state;
+    return (
+      <div className="header">
+        <h1>STAR WARS</h1>
+        <nav>
+          <ul>
+            {routes.map((route) => (
+              <li key={route.item}>
+                <a href={route.path}>{route.item}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
