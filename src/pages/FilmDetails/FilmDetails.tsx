d5e50c94c2a53d1f5c28ca40c3178d66d8dff62a
Matheus Santos <matheus.santos@ilegra.com>
2019-12-03 17:15:56 -0300

    Improved FilmDetails component.

--- src/pages/FilmDetails/FilmDetails.tsx
+++ src/pages/FilmDetails/FilmDetails.tsx
@@ -2,82 +2,97 @@ import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import './FilmDetails.scss';
 import { FilmModel } from '../../models/FilmModel';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { CharacterModel } from '../../models/CharacterModel';
 
 export default function FilmDetails(props: any) {
+  const [details, setDetails] = useState();
+  const [charactersDetails, setCharactersDetails] = useState();
+  const [characters, setCharacters] = useState();
+
   useEffect(() => {
+    const loadDetails = async () => {
+      const response = await api.get(`films/`);
+      const { results } = response.data;
+      const films = results.find((film: FilmModel) => {
+        return `${film.episode_id}` === `${props.match.params.id}`;
+      });
+
+      setDetails(films);
+    };
+
     loadDetails();
-    loadCharacters();
   }, []);
 
-  const [details, setDetails] = useState({
-    episode_id: '',
-    title: '',
-    producer: '',
-    director: '',
-    release_date: '',
-    characters: [],
-    planets: [],
-    starships: [],
-    vehicles: [],
-    species: []
-  });
-
-  const [characters, setCharacters] = useState([]);
-
-  const loadDetails = async () => {
-    const response = await api.get(`films/`);
-    const { results } = response.data;
-
-    const films = results.find((film: FilmModel) => {
-      return `${film.episode_id}` === `${props.match.params.id}`;
-    });
-    setDetails(films);
-  };
-
-  const loadCharacters = async () => {
-    const response = await api.get(`people/`);
-    const { results } = response.data;
-
-    const characters = results.filter((character: CharacterModel) => {
-      if (character.films.toString().includes(`${window.location.pathname}`)) {
-        return `${character.name}`;
+  useEffect(() => {
+    const buildCharactersPath = () => {
+      if (!details) {
+        return;
       }
-    });
 
-    setCharacters(characters);
-  };
+      const charactersPath = details.characters.map(
+        (character: CharacterModel) => {
+          const charPath = character.toString().substr(21);
+          return charPath;
+        }
+      );
+
+      setCharactersDetails(charactersPath);
+    };
+
+    buildCharactersPath();
+  }, [details]);
+
+  useEffect(() => {
+    const loadCharacters = async () => {
+      if (!charactersDetails) {
+        return;
+      }
+
+      const characters = charactersDetails.map(async (character: string) => {
+        const response = await api.get(character);
+        const { data } = response;
+
+        setCharacters((state: []) => [...state, data]);
+        return data;
+      });
+
+      setCharacters(characters);
+    };
+
+    loadCharacters();
+  }, [charactersDetails]);
 
   return (
-    <ul className="film-details">
-      <li>{`Episode ${details.episode_id} - ${details.title}`}</li>
-      <li>{`Producer: ${details.producer}`}</li>
-      <li>{`Director: ${details.director}`}</li>
-      <li>{`Release_date: ${details.release_date}`}</li>
+    <div>
       <ul>
-        {characters.map((character: CharacterModel) => (
-          <li key={character.name} className="character-name">
-            {`${character.name}`}
-          </li>
-        ))}
+        {characters &&
+          characters.map((character: CharacterModel) => (
+            <li key={character.name}>{character.name}</li>
+          ))}
       </ul>
-
-      <li>
-        <Link to={`${details.episode_id}/people/`}>Characters</Link>
-      </li>
-      <li>
-        <Link to={`${details.episode_id}/planets/`}>Planets</Link>
-      </li>
-      <li>
-        <Link to={`${details.episode_id}/species/`}>Species</Link>
-      </li>
-      <li>
-        <Link to={`${details.episode_id}/vehicles/`}>Vehicles</Link>
-      </li>
-      <li>
-        <Link to={`${details.episode_id}/starships/`}>Starships</Link>
-      </li>
-    </ul>
+    </div>
+    // <ul className="film-details">
+    //   <li>{`Episode ${details.episode_id} - ${details.title}`}</li>
+    //   <li>{`Producer: ${details.producer}`}</li>
+    //   <li>{`Director: ${details.director}`}</li>
+    //   <li>{`Release_date: ${details.release_date}`}</li>
+    //   <li>{}</li>
+    //   <li>
+    //     <Link to={`${details.episode_id}/people/`}>Characters</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={`${details.episode_id}/planets/`}>Planets</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={`${details.episode_id}/species/`}>Species</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={`${details.episode_id}/vehicles/`}>Vehicles</Link>
+    //   </li>
+    //   <li>
+    //     <Link to={`${details.episode_id}/starships/`}>Starships</Link>
+    //   </li>
+    // </ul>
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import FilmDetails from './pages/FilmDetails/FilmDetails';
 import Character from './pages/Characters/Characters';
-import Planet from './pages/Planets/Planets';
 import Vehicle from './pages/Vehicles/Vehicles';
 import Specie from './pages/Species/Species';
 import Starship from './pages/Starships/Starships';
@@ -14,13 +13,11 @@ const Routes = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/films" component={Home} />
       <Route exact path="/characters" component={Character} />
-      <Route exact path="/planets" component={Planet} />
       <Route exact path="/vehicles" component={Vehicle} />
       <Route exact path="/species" component={Specie} />
       <Route exact path="/starships" component={Starship} />
       <Route exact path="/films/:id" component={FilmDetails} />
       <Route exact path="/films/:id/characters" component={Character} />
-      <Route exact path="/films/:id/planets" component={Planet} />
       <Route exact path="/films/:id/vehicles" component={Vehicle} />
       <Route exact path="/films/:id/species" component={Specie} />
       <Route exact path="/films/:id/starships" component={Starship} />
